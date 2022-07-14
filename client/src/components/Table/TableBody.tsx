f67b05168b8719fa84c42ea1be7d114ca9f65494
shlomi-atzmon <shlomiatzmon82@gmail.com>
2022-07-14 11:21:40 +0300

    IntersectionObserver + react-query tests

--- client/src/components/Table/TableBody.tsx
+++ client/src/components/Table/TableBody.tsx
@@ -9,47 +9,72 @@ interface Props {
   path: string,
 }
 
-const limit = 30;
-const ItemsOnScreen = 150;
+const itemsPerPage = 20;
+const maxItemsOnScreen = 100;
 
 const TableBody = ({ path }: Props) => {
   const [editRowId, setEditRowId] = useState<string | null>(null);
   const [direction, setDirection] = useState<"down" | "up">("down");
   const [page, setPage] = useState<number>(1);
+  const [rows, setRows] = useState([])
+
+  //const [lastPage, setLastPage] = useState<number>(1);
+
+  const lastPage = useRef<number>(1);
+
+
+
+  const firstPage = (lastPage.current >= 5) ? lastPage.current - 5 : 1
+  console.log('==> firstPage : ' + firstPage);
+  console.log("==> lastPage : " + lastPage.current);
 
   /// Fetch Data Section /// 
-  const fetchData = async (page: number) => {
-
-    if (direction === 'up') console.log(page)
-
-
-
-    const response = await moonactive.request({
-      url: path,
-      method: 'GET',
-      params: { page, limit }
-    });
+  const dataHandler = (data: any) => {
 
     // data.results => old data
     // response.data.results => new data
-    if (data) {
 
-      let newData = [...data.results, ...response.data.results];
+    //console.log(direction)
 
+    let newData = data.results;
 
-      if (direction === "down") {
-        //console.log(data.results.length >= ItemsOnScreen, data.results.length, ItemsOnScreen);
-        if (data.results.length >= ItemsOnScreen) {
-          newData = newData.filter((item: any, index: number) => index > (limit - 1));
-          console.log(newData);
-        }
+    if (direction === "down") {
+
+      newData = [...rows, ...data.results];
+      console.log("D: Adding page: " + page);
+
+      if (lastPage.current > maxItemsOnScreen / itemsPerPage) { //(data.results.length >= maxItemsOnScreen) {
+        newData = newData.filter((item: any, index: number) => index > (itemsPerPage - 1));
+        //console.log(newData);
+
+        console.log("D: Removing page: " + firstPage);
+
       }
-
-
-      response.data.results = newData;
     }
 
-    //console.log(response.data)
+
+    console.log("==== This stinks: ====")
+    console.log("Direction: " + direction)
+    console.log("firstPage: " + firstPage)
+    if (direction === 'up') {
+
+      console.log("U: Adding page: " + firstPage);
+      newData = [...data.results, ...rows];
+
+      console.log("U: Removing page: " + (lastPage.current));
+      newData = newData.filter((item: any, index: number) => index < maxItemsOnScreen);
+    }
+
+
+    setRows(newData);
+  }
+
+  const fetchData = async (page: number) => {
+    const response = await moonactive.request({
+      url: path,
+      method: 'GET',
+      params: { page, limit: itemsPerPage }
+    });
     return response.data;
   }
 
@@ -60,6 +85,7 @@ const TableBody = ({ path }: Props) => {
   } = useQuery(['promotion', page], () => fetchData(page), {
     keepPreviousData: true,
     refetchOnWindowFocus: false,
+    onSuccess: (data) => dataHandler(data)
   });
   /// End Fetch Data Section /// 
 
@@ -75,15 +101,25 @@ const TableBody = ({ path }: Props) => {
     if (observe.first.current) observe.first.current.disconnect();
 
     observe.first.current = new IntersectionObserver(entries => {
+
       if (entries[0].isIntersecting && data.previousPage) {
+
         setDirection("up");
-        /* if (data.previousPage > 4) setPage(data.previousPage - 4); */
-        setPage(data.previousPage)
+
+        if (lastPage.current > 5) {
+          console.log("Up observer - last page: " + lastPage.current)
+          setPage(firstPage)
+          lastPage.current = lastPage.current - 1
+        }
+
       }
+    }, {
+      root: document.querySelector(".t-body"),
+      rootMargin: "400px 0px 0px 0px"
     });
 
     if (node) observe.first.current.observe(node)
-  }, [isLoading, data?.previousPage, observe.first]);
+  }, [isLoading, data?.previousPage, observe.first, firstPage]);
 
   const lastItemRef = useCallback((node: HTMLDivElement) => {
     if (isLoading) return;
@@ -92,9 +128,10 @@ const TableBody = ({ path }: Props) => {
     observe.last.current = new IntersectionObserver(entries => {
       if (entries[0].isIntersecting && data.nextPage) {
         setDirection("down");
-        setPage(data.nextPage);
-        //console.log("Data Next Page: " + data.nextPage)
-        //console.log("Page: " + page)
+
+        setPage(lastPage.current + 1);
+        lastPage.current = lastPage.current + 1
+
 
       }
     }, {
@@ -108,12 +145,12 @@ const TableBody = ({ path }: Props) => {
 
   /// Render Section 
   const serializeRows = () => {
-    return data.results.map((item: Promotion, i: number) => {
+    return rows.map((item: Promotion, i: number) => {
 
       // Set observers on DOM
       const refProp: React.Attributes | {} = i === 0
         ? { ref: firstItemRef }
-        : data.results.length === i + 1
+        : rows.length === i + 1
           ? { ref: lastItemRef }
           : {}
 
@@ -149,7 +186,9 @@ const TableBody = ({ path }: Props) => {
         <div className='t-row'>
           <div className='t-col'>Error...</div>
         </div>
-      ) : <div className='t-body' style={{ width: '100%' }}>{serializeRows()}</div>
+      ) : <div className='t-body'>
+        {serializeRows()}
+      </div>
       }
     </>
   )
--- client/src/hooks/use-http.tsx
+++ client/src/hooks/use-http.tsx
@@ -1,15 +1,14 @@
 import { httpConfig } from '../types/http-config';
 import moonactive from "../api/moonactive";
+import { toast } from 'react-toastify';
 import { useState } from "react";
 
 const useHttp = ({ url, method, data, onSuccess }: httpConfig) => {
-  const [errors, setErrors] = useState<JSX.Element | null>(null);
   const [loading, setLoading] = useState(false);
 
   const sendRequest = async () => {
     try {
       setLoading(true);
-      setErrors(null);
 
       const response = await moonactive.request({ method, url, data });
 
@@ -18,24 +17,22 @@ const useHttp = ({ url, method, data, onSuccess }: httpConfig) => {
       }
 
       // TODO : Add err type  
-    } catch (err: any) {
-      setErrors(
-        <div role='alert'>
-          <strong>Ho snap!</strong>
-          <p>
-            {err.response.data.errors.map((err: any) => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </p>
-          <strong>TRY AGIAN</strong>
+    } catch (e: any) {
+      toast.error((
+        <div>{
+          e.response.data.messages.map((err: any) => (
+            <li className="p-1" key={err}>{err}</li>
+          ))}
         </div>
-      );
+      ), {
+        position: toast.POSITION.TOP_LEFT
+      });
     }
 
     setLoading(false);
   }
 
-  return { sendRequest, errors, loading };
+  return { sendRequest, loading };
 }
 
 export default useHttp;
